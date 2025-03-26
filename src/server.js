18a5134e29c0020dbeca309893030c70fe9c0941
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-26 10:49:28 -0300

    Executando Typescript com tsx

--- src/server.js
+++
@@ -1,6 +0,0 @@
-function sum(a, b) {
-    return a + b;
-}
-;
-var result = sum(5, 3);
-console.log("Resultado da soma", result);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-function sum(a, b) {
+function sum(a: number, b: number) {
   return a + b;
 };
 
-const result: number = sum(5, 3);
-console.log("Resultado da soma", result);
+const sumResult: number = sum(5, 3);
+console.log("Resultado da", sumResult);
