6419aabef00c4aed80c0309698ba1e087eadb109
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-26 21:53:35 -0300

    Resposta com JSON e Status Code

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.get("/products", (request, response) => {
 app.post("/products", (request, response) => {
   const { name, price } = request.body;
 
-  response.send(`Produto ${name} custa $ ${price}`);
+  response.status(201).json({ name, price });
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
