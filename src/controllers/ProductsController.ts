a879fa1084bd49c5fddc34930347b191b2c4cae0
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-27 10:33:44 -0300

    Tratando exceções

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -18,6 +18,8 @@ class ProductsController {
   create(request: Request, response: Response) {
     const { name, price } = request.body;
 
+    throw new Error("ERROR DE EXEMPLO!");
+
     response.status(201).json({ name, price, user_id: request.user_id });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { routes } from './routes';
 
 const PORT = 3333;
@@ -7,5 +7,11 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  response.status(500).json({
+    message: error.message
+  });
+})
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
