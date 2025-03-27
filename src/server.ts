a1d982883749d7e94061e17e82928ca3bd717941
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-26 21:31:52 -0300

    Enviando requisição com o método GET

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
-const PORT = 3333
+const PORT = 3333;
 
-const app = express()
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello World Express!");
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
