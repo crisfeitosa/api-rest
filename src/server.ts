868afcd20fe901c254b77518585cc291a505a484
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-26 21:51:22 -0300

    Dados no corpo da requisição

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,18 @@ import express from 'express';
 const PORT = 3333;
 
 const app = express();
+app.use(express.json());
 
 app.get("/products", (request, response) => {
   const { page, limit } = request.query;
 
   response.send(`Página ${page} de ${limit}`);
+});
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body;
+
+  response.send(`Produto ${name} custa $ ${price}`);
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
