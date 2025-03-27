307980e6a6f4ea54142cafa38ceaabe80c2c3921
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-27 10:08:39 -0300

    Controller

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,20 +1,14 @@
 import { Router } from "express";
 import { myMiddleware } from "../middlewares/myMiddleware";
+import { ProductsController } from "../controllers/ProductsController";
 
 const productsRoutes = Router();
+const productsController = new ProductsController();
 
-productsRoutes.get("/", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Página ${page} de ${limit}`);
-});
+productsRoutes.get("/", productsController.index);
 
 // Middleware local em uma rota especifica
-productsRoutes.post("/", myMiddleware, (request, response) => {
-  const { name, price } = request.body;
-
-  response.status(201).json({ name, price, user_id: request.user_id });
-});
+productsRoutes.post("/", myMiddleware, productsController.create);
 
 export { productsRoutes }
 
